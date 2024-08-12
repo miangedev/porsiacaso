b79ec6638d883446d41032be5291e369e4fce4f3
aftermath0240 <145938864+aftermath0240@users.noreply.github.com>
2024-08-11 20:03:05 -0500

    Add files via upload

---
+++ 111.js
@@ -0,0 +1,5 @@
+const image1 = document.getElementById('melissa');
+
+botonImagen.addEventListener('click', () => {
+    window.location.href = '    https://drive.google.com/drive/folders/1-dMFd5fuXL_Uue9wcg3UW-KOSfaX7kMJ?usp=sharing';
+});
